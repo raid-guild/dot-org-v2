1bcad94033e7cbf90c075c8b87f39a6478a12902
Mano <manolingam1996@gmail.com>
2021-06-11 20:13:00 +0530

    some more updates

--- components/Token.jsx
+++ components/Token.jsx
@@ -1,4 +1,6 @@
-import { Flex, Heading, Link, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text } from '@chakra-ui/react';
+
+import Link from 'next/link';
 
 export const Token = () => {
   return (
@@ -31,16 +33,24 @@ export const Token = () => {
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat.
       </Text>
-      <Link
+      <Button
+        h='50px'
+        borderRadius='none'
         fontFamily='spaceMono'
-        fontSize='1.5rem'
+        fontSize={{ base: '1rem', lg: '1.5rem' }}
         p='1rem'
         color='white'
         fontWeight='bold'
         bg='linear-gradient(153deg, rgba(255,56,100,1) 0%, rgba(130,46,166,1) 49%, rgba(242,232,87,1) 100%)'
+        _hover={{
+          background:
+            'linear-gradient(153deg, #dd2049 0%, #5e177c 49%, #bdb332 100%)'
+        }}
       >
-        Learn more
-      </Link>
+        <Link href='/'>
+          <a>Learn more</a>
+        </Link>
+      </Button>
     </Flex>
   );
 };
