ff446939a49c117aa7af46c25a18fd38ca2831a1
Mano <manolingam1996@gmail.com>
2022-03-15 21:28:36 +0530

    Update Intro.jsx

--- components/joinus/Intro.jsx
+++ components/joinus/Intro.jsx
@@ -1,7 +1,14 @@
 import React, { useContext } from 'react';
-import { Flex, Heading, Text, Button } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 
 import { AppContext } from '../../context/AppContext';
+
+import {
+  StyledPrimaryButton,
+  StyledPrimaryHeading,
+  StyledBodyText,
+  StyledMessageText
+} from '../../themes/styled';
 
 export const Intro = () => {
   const context = useContext(AppContext);
@@ -13,41 +20,62 @@ export const Intro = () => {
         px={{ base: '1rem', lg: '4rem' }}
         mx='1rem'
       >
-        <Heading
-          variant='headingTwo'
+        <StyledPrimaryHeading
           fontSize={{ base: '1.5rem', lg: '36px' }}
           mb='1rem'
         >
           Apply to Join RaidGuild
-        </Heading>
+        </StyledPrimaryHeading>
 
-        <Text variant='textOne' fontSize={{ base: '1rem', lg: '18px' }}>
+        <StyledBodyText fontSize={{ base: '1rem', lg: '18px' }}>
           Humans wanted for hazardous journey into the ether. Smol wages to
           start, but a lifetime of rewards. Bitter cold winters to build,
           glorious summers to reap. Long months of navigating the dark forest.
           Constant danger lurking in the mempool. Safe return to normalcy
           doubtful. Great honor and recognition in case of success.
-        </Text>
+        </StyledBodyText>
         <br />
 
-        <Text variant='textOne' fontSize={{ base: '1rem', lg: '18px' }}>
+        <StyledBodyText fontSize={{ base: '1rem', lg: '18px' }}>
           Your path is marked by this first command - fill this form to apply to
           RaidGuild firsthand. Pledges are studied by our counsel forth. Last,
           not least, we'll invite you to join a training cohort in due course.
-        </Text>
+        </StyledBodyText>
 
         <br />
+        <Flex
+          direction='column'
+          bgColor='white'
+          borderRadius='0.5rem'
+          p='0.5rem 1rem'
+          maxW='720px'
+        >
+          <StyledMessageText
+            fontSize={{ base: '1rem', lg: '18px' }}
+            fontWeight='bold'
+            mb='.5rem'
+          >
+            Status: Season 3
+          </StyledMessageText>
+          <StyledMessageText fontSize={{ base: '1rem', lg: '16px' }}>
+            Cohorts are being trained & building cool stuff. Applications are
+            closed for season 3 but open for next season. Selected applicants
+            will be notified via email when it's time.
+          </StyledMessageText>
+        </Flex>
+        <br />
 
-        <Button
-          variant='primary'
+        <StyledPrimaryButton
           fontSize={{ base: '16px', lg: '18px' }}
           onClick={() => {
             context.updateStage('next');
           }}
         >
           Start Application
-        </Button>
+        </StyledPrimaryButton>
       </Flex>
     </>
   );
+};
+
 };
