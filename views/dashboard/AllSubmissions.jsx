252cee6291fb4c7307dbad399a273658bb9c2295
Manolingam N <manolingam1996@gmail.com>
2022-03-15 22:25:31 +0530

    link fix for bids

--- views/dashboard/AllSubmissions.jsx
+++ views/dashboard/AllSubmissions.jsx
@@ -261,7 +261,7 @@ export const AllSubmissions = ({ clientInfo, getClientInfo }) => {
                     textDecoration='underline'
                     onClick={() =>
                       window.open(
-                        `hhttps://bids.raidguild.org/bids/${item['fields']['Submission Hash']}`,
+                        `https://bids.raidguild.org/bids/${item['fields']['Submission Hash']}`,
                         '_blank'
                       )
                     }
