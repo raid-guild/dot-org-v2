b4a008215f6d795984c0ee338dfc4c74cff63444
Han Valen <han@0xhanvalen.com>
2022-11-28 10:40:23 -0800

    added paginated results, edit/view buttons, link destinations for portfolio/blog to admin panel

--- pages/admin.js
+++ pages/admin.js
@@ -1,10 +1,12 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image, HStack, VStack, Link } from "@chakra-ui/react";
 
 import CMSPageTemplate from "../components/page-templates/CMSPageTemplate";
 import PageTitle from "../components/page-components/PageTitle";
 import supabase from "../shared/Supabase";
 
 export default function Admin(props) {
+  console.log(props?.data);
+
   return (
     <CMSPageTemplate>
       <Box>
@@ -25,28 +27,42 @@ export default function Admin(props) {
               margin: `3rem auto`,
             }}
           >
-            <Box
-              sx={{
-                backgroundColor: `white`,
-                color: `red`,
-                padding: `.5rem`,
-                borderRadius: `0.25rem`,
-                fontWeight: `700`,
-              }}
-            >
-              Add Portfolio Project
-            </Box>
-            <Box
-              sx={{
-                backgroundColor: `white`,
-                color: `red`,
-                padding: `.5rem`,
-                borderRadius: `0.25rem`,
-                fontWeight: `700`,
-              }}
-            >
-              Add Blog Post
-            </Box>
+            <Link href="/portfolio/shipping-station">
+              <Box
+                sx={{
+                  backgroundColor: `white`,
+                  color: `red`,
+                  padding: `.5rem`,
+                  borderRadius: `0.25rem`,
+                  fontWeight: `700`,
+                }}
+                _hover={{
+                  color: `white`,
+                  backgroundColor: `purple`,
+                  cursor: `pointer`,
+                }}
+              >
+                Add Portfolio Project
+              </Box>
+            </Link>
+            <Link href="/state-of-the-raid/publish">
+              <Box
+                sx={{
+                  backgroundColor: `white`,
+                  color: `red`,
+                  padding: `.5rem`,
+                  borderRadius: `0.25rem`,
+                  fontWeight: `700`,
+                }}
+                _hover={{
+                  color: `white`,
+                  backgroundColor: `purple`,
+                  cursor: `pointer`,
+                }}
+              >
+                Add Blog Post
+              </Box>
+            </Link>
           </Box>
           <Box
             sx={{
@@ -56,11 +72,152 @@ export default function Admin(props) {
               minWidth: `400px`,
             }}
           >
-            <Box sx={{ display: `flex`, flexDirection: `column` }}>Meow</Box>
-            <Box sx={{ display: `flex`, flexDirection: `column` }}>Meow</Box>
+            <Box sx={{ display: `flex`, flexDirection: `column` }}>
+              {props.data.portfolioContent.data.map((content, index) => {
+                let date = new Date(content.created_at);
+                let dateString = date.toDateString();
+                return (
+                  <Box
+                    sx={{
+                      backgroundColor: `blackLight`,
+                      padding: `1rem`,
+                      margin: `1rem`,
+                      borderRadius: `0.5rem`,
+                    }}
+                    key={`p-${index}`}
+                  >
+                    <HStack>
+                      <Image src={content?.image_url} sx={{ width: `48px` }} />
+                      <VStack align="flex-start">
+                        <h2 style={{ fontSize: `1.66rem`, fontWeight: `600` }}>
+                          {content.project_name}
+                        </h2>
+                        <h3>{dateString}</h3>
+                        <HStack>
+                          <Link
+                            href={`/portfolio/${content.project_name}/edit`}
+                          >
+                            <Box
+                              sx={{
+                                backgroundColor: `red`,
+                                padding: `0.25rem 1rem`,
+                                borderRadius: `0.5rem`,
+                              }}
+                              _hover={{
+                                backgroundColor: `purple`,
+                                color: `white`,
+                                cursor: `pointer`,
+                              }}
+                            >
+                              Edit
+                            </Box>
+                          </Link>
+                          <Link href={`/portfolio/${content.project_name}`}>
+                            <Box
+                              sx={{
+                                backgroundColor: `white`,
+                                color: `red`,
+                                padding: `0.25rem 1rem`,
+                                borderRadius: `0.5rem`,
+                              }}
+                              _hover={{
+                                backgroundColor: `purple`,
+                                color: `white`,
+                                cursor: `pointer`,
+                              }}
+                            >
+                              View
+                            </Box>
+                          </Link>
+                        </HStack>
+                      </VStack>
+                    </HStack>
+                  </Box>
+                );
+              })}
+            </Box>
+            <Box sx={{ display: `flex`, flexDirection: `column` }}>
+              {props.data.blogContent.data.map((content, index) => {
+                let date = new Date(content.created_at);
+                let dateString = date.toDateString();
+                console.log(content);
+                return (
+                  <Box
+                    sx={{
+                      backgroundColor: `blackLight`,
+                      padding: `1rem`,
+                      margin: `1rem`,
+                      borderRadius: `0.5rem`,
+                    }}
+                    key={`p-${index}`}
+                  >
+                    <HStack>
+                      <VStack align="flex-start">
+                        <h2 style={{ fontSize: `1.66rem`, fontWeight: `600` }}>
+                          {content.post_title}
+                        </h2>
+                        <h3>{dateString}</h3>
+                        <HStack>
+                          <Link
+                            href={`/state-of-the-raid/${content.post_title}/edit`}
+                          >
+                            <Box
+                              sx={{
+                                backgroundColor: `red`,
+                                padding: `0.25rem 1rem`,
+                                borderRadius: `0.5rem`,
+                              }}
+                              _hover={{
+                                backgroundColor: `purple`,
+                                color: `white`,
+                                cursor: `pointer`,
+                              }}
+                            >
+                              Edit
+                            </Box>
+                          </Link>
+                          <Link
+                            href={`/state-of-the-raid/${content.post_title}`}
+                          >
+                            <Box
+                              sx={{
+                                backgroundColor: `white`,
+                                color: `red`,
+                                padding: `0.25rem 1rem`,
+                                borderRadius: `0.5rem`,
+                              }}
+                              _hover={{
+                                backgroundColor: `purple`,
+                                color: `white`,
+                                cursor: `pointer`,
+                              }}
+                            >
+                              View
+                            </Box>
+                          </Link>
+                        </HStack>
+                      </VStack>
+                    </HStack>
+                  </Box>
+                );
+              })}
+            </Box>
           </Box>
         </Box>
       </Box>
     </CMSPageTemplate>
   );
+}
+
+export async function getServerSideProps(context) {
+  let portfolioContent = await supabase
+    .from("PortfolioContent")
+    .select("*")
+    .range(0, 9);
+
+  let blogContent = await supabase.from("BlogContent").select("*").range(0, 9);
+
+  return {
+    props: { data: { portfolioContent, blogContent } },
+  };
 }
