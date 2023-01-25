4c30849deb936938e74bf99b80988c840d318318
Scott Herren <me@stwoh.com>
2023-01-25 17:34:48 -0600

    infura provider

--- utils/chains.ts
+++ utils/chains.ts
@@ -1,13 +1,13 @@
 import { configureChains } from 'wagmi';
 import { mainnet, polygon, arbitrum, optimism, goerli, sepolia, gnosis } from 'wagmi/chains';
-// import { infuraProvider } from 'wagmi/providers/infura';
+import { infuraProvider } from 'wagmi/providers/infura';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
 export const { chains, provider } = configureChains(
   [gnosis, mainnet, polygon, arbitrum, optimism, goerli, sepolia],
   [
-    // infuraProvider({ apiKey: process.env.NEXT_PUBLIC_RPC_KEY || "" }),
+    infuraProvider({ apiKey: process.env.NEXT_PUBLIC_RPC_KEY || '' }),
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY || '' }),
     jsonRpcProvider({
       rpc: (localChain: any) => ({
