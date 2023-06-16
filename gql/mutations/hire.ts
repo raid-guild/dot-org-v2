a14cb350e5fad7917335d7203c8f353345a22120
Scott Herren <me@stwoh.com>
2023-06-16 17:51:17 -0500

    fix application

--- gql/mutations/hire.ts
+++ gql/mutations/hire.ts
@@ -1,10 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import { gql } from 'graphql-request';
 
-// @Dev: do not use 'CONSULTATION_CREATE_MUTATION' with 'consultation_insert_input'
-// this query is not callable for non RaidGuild member wallets.
+// @Dev: do not use 'insert_consultations_one'
+// this query is not callable for non RaidGuild member/cohort wallets.
 // Hasura doesn't let you use insert_models_one unless the role (user) has
-// permissions to select ids at least. We'll want to use insert_models in any case like this.
+// permissions to select ids at least.
+// We'll want to use insert_models in any case like this.
 
 export const CONSULTATIONS_CREATE_MUTATION = gql`
   mutation ConsultationsInsertMutation($consultations: consultations_insert_input!) {
--- gql/mutations/join.ts
+++ gql/mutations/join.ts
@@ -1,9 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 import { gql } from 'graphql-request';
 
+// @Dev: do not use 'insert_applications_one'
+// this query is not callable for non RaidGuild member/cohort wallets.
+// Hasura doesn't let you use insert_models_one unless the role (user) has
+// permissions to select ids at least.
+// We'll want to use insert_models in any case like this.
+
 export const APPLICATION_CREATE_MUTATION = gql`
   mutation ApplicationInsertMutation($application: applications_insert_input!) {
-    insert_applications_one(object: $application) {
+    insert_applications(objects: [$application]) {
       id
     }
   }
