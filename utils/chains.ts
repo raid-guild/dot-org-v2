1281dab62c6d5918a1679017cbf3369bdf234911
nitegeist <nitegeist.dev@gmail.com>
2023-08-15 17:40:55 -0500

    Upgrade to wallet connect v2

--- utils/chains.ts
+++ utils/chains.ts
@@ -1,18 +1,14 @@
 import { configureChains } from 'wagmi';
-import { mainnet, polygon, arbitrum, optimism, goerli, sepolia, gnosis } from 'wagmi/chains';
-import { infuraProvider } from 'wagmi/providers/infura';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { arbitrum, gnosis, goerli, mainnet, optimism, polygon, sepolia } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { infuraProvider } from 'wagmi/providers/infura';
+import { publicProvider } from 'wagmi/providers/public';
 
-export const { chains, provider } = configureChains(
+export const { chains, publicClient, webSocketPublicClient } = configureChains(
   [gnosis, mainnet, polygon, arbitrum, optimism, goerli, sepolia],
   [
-    infuraProvider({ apiKey: process.env.NEXT_PUBLIC_RPC_KEY || '' }),
+    infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_ID || '' }),
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY || '' }),
-    jsonRpcProvider({
-      rpc: (localChain: any) => ({
-        http: localChain.rpcUrls.default,
-      }),
-    }),
+    publicProvider(),
   ],
 );
--- utils/wagmiClient.ts
+++ utils/wagmiClient.ts
@@ -1,18 +1,18 @@
 /* eslint-disable import/prefer-default-export */
-import { createClient } from 'wagmi';
 import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import {
-  injectedWallet,
-  metaMaskWallet,
-  rainbowWallet,
-  walletConnectWallet,
   argentWallet,
   braveWallet,
   coinbaseWallet,
+  injectedWallet,
   ledgerWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  walletConnectWallet,
 } from '@rainbow-me/rainbowkit/wallets';
+import { createConfig } from 'wagmi';
 
-import { chains, provider } from './chains';
+import { chains, publicClient } from './chains';
 
 const connectors = connectorsForWallets([
   {
@@ -20,24 +20,24 @@ const connectors = connectorsForWallets([
 
     wallets: [
       injectedWallet({ chains }),
-      metaMaskWallet({ chains, shimDisconnect: false }),
-      walletConnectWallet({ chains }),
-      ledgerWallet({ chains }),
+      metaMaskWallet({ chains, projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '', shimDisconnect: false }),
+      walletConnectWallet({ chains, projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '' }),
+      ledgerWallet({ chains, projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '' }),
     ],
   },
   {
     groupName: 'Others',
     wallets: [
-      rainbowWallet({ chains }),
+      rainbowWallet({ chains, projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '' }),
       coinbaseWallet({ chains, appName: 'Dungeon Master' }),
-      argentWallet({ chains }),
+      argentWallet({ chains, projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '' }),
       braveWallet({ chains }),
     ],
   },
 ]);
 
-export const wagmiClient = createClient({
-  provider,
+export const wagmiClient = createConfig({
+  publicClient,
   connectors,
   // turn off autoConnect in development
   autoConnect: true,
