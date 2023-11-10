a8cfbfa27a9cf67a0366fcff45ea35108d937d9f
memosys <hello@vishal.design>
2023-11-10 15:43:31 +0530

    Update Markdown.tsx

--- components/atoms/Markdown.tsx
+++ components/atoms/Markdown.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
-import { defaultTheme } from '@raidguild/design-system';
+import { Text } from '@raidguild/design-system';
+
+const newTheme = {
+  p: (props:any) => {
+    const { children } = props;
+    return <Text color='white'>{children}</Text>;
+  },
+};
 
 type MarkdownProps = {
   children: string;
 };
 
 const Markdown = ({ children }: MarkdownProps) => (
-  <ReactMarkdown components={ChakraUIRenderer(defaultTheme)} remarkPlugins={[remarkGfm]}>
+  <ReactMarkdown components={ChakraUIRenderer(newTheme)} remarkPlugins={[remarkGfm]} skipHtml>
     {children}
   </ReactMarkdown>
 );
