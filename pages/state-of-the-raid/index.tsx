8c71434bb45e62242421bc1e51d7243c795be499
memosys <hello@vishal.design>
2023-11-25 01:03:41 +0530

    filters update

--- pages/state-of-the-raid/index.tsx
+++ pages/state-of-the-raid/index.tsx
@@ -10,12 +10,13 @@ import {
   Text,
   VStack,
   defaultTheme,
+  useBreakpointValue,
 } from '@raidguild/design-system';
 import _ from 'lodash';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { FiEdit } from 'react-icons/fi';
 import { useEffect, useState } from 'react';
+import { FiEdit, FiFilter } from 'react-icons/fi';
 import wallSconce from '../../assets/illustrations/wallSconce.svg';
 import Link from '../../components/atoms/ChakraNextLink';
 import GradientBorderButton from '../../components/atoms/GradientBorderButton';
@@ -27,7 +28,7 @@ import useBlogsList from '../../hooks/useBlogsList';
 import { checkPermission } from '../../utils';
 import tokens from '../../utils/extendedTokens';
 
-const Topics = ['Raid', 'Guild', 'DAO', 'Community'];
+const Topics = ['', 'Raid', 'Guild', 'Community'];
 interface PostProps {
   post: any;
 }
@@ -35,8 +36,8 @@ interface PostProps {
 const Post = ({ post }: PostProps) => {
   const link = `/state-of-the-raid/${post.slug}`;
   return (
-    <Link href={link}>
-      <Card border='none' w={{ base: '100%', md: '70rem' }} mx='auto' py={4} px={2}>
+    <Link href={link} w='100%'>
+      <Card py={4} px={2} _hover={{ bg: '#FFFFFF08' }} border='none'>
         <Flex minH='250px' align='center'>
           <Stack
             spacing='2rem'
@@ -45,7 +46,7 @@ const Post = ({ post }: PostProps) => {
             textAlign={{ base: 'center', md: 'left' }}>
             <Image src={_.get(post, 'imageUrl', wallSconce.src)} width='200px' height='250px' />
             <VStack maxWidth='50ch' spacing={6} color='white' align={{ base: 'center', md: 'start' }}>
-              <Heading fontSize='3xl' fontFamily='texturina'>
+              <Heading fontSize='2xl' fontFamily='texturina'>
                 {_.get(post, 'title')}
               </Heading>
               <Text noOfLines={3}>{_.get(post, 'description')}</Text>
@@ -86,6 +87,8 @@ const AllPosts = ({ initialData }: Props) => {
   const canCreate = checkPermission(session);
   const router = useRouter();
 
+  const [hideFilters, setHideFilters] = useState(false);
+  const isXL = useBreakpointValue({ base: false, xl: true });
   const [filteredblogs, setFilteredblogs] = useState(blogs);
   const { q } = router.query;
 
@@ -115,31 +118,77 @@ const AllPosts = ({ initialData }: Props) => {
             />
           </Stack>
         )}
-        <HStack placeItems='flex-start'>
-          <VStack mt={16} alignItems={{ base: 'center' }} spacing={20}>
+        <Stack
+          justify='center'
+          align='flex-start'
+          flexDir={{ base: 'column-reverse', xl: 'row' }}
+          maxW='100vw'
+          gap={20}
+          p={10}>
+          <VStack mt={16} alignItems={{ base: 'center' }} spacing={10}>
             {_.map(filteredblogs, (post) => (
               <Post post={post} key={_.get(post, 'slug')} />
             ))}
           </VStack>
-          <VStack border={`1px solid ${defaultTheme.colors.primary[500]}`} p={16} placeItems='flex-start' mt='5%'>
-            <Heading fontSize='3xl' fontFamily='texturina'>
-              Topics
-            </Heading>
-            <ul>
-              {Topics.map((topic) => (
-                <li key={topic}>
-                  <Text
-                    mt={3}
-                    pl={4}
-                    onClick={() => router.push(`/state-of-the-raid?q=${topic.toLowerCase()}`)}
-                    _hover={{ cursor: 'pointer' }}>
-                    {topic}
-                  </Text>
-                </li>
-              ))}
-            </ul>
+          <VStack
+            border={`1px solid ${defaultTheme.colors.primary[500]}`}
+            p={{ base: 0, xl: 8 }}
+            mt={16}
+            gap={4}
+            justify='flex-start'
+            align='flex-start'
+            w={{ base: '100%', xl: '25%' }}>
+            <HStack
+              spacing={4}
+              align='center'
+              w='full'
+              justify='space-between'
+              p={4}
+              hideFrom='xl'
+              onClick={() => setHideFilters(!hideFilters)}
+              _hover={{ cursor: 'pointer' }}>
+              <Text color='white' fontSize='md' fontFamily='texturina'>
+                Filters
+              </Text>
+              <FiFilter fontSize={18} color='white' />
+            </HStack>
+
+            <Box ml={6} mb={8} px={8} hidden={hideFilters && !isXL}>
+              <Heading fontSize='3xl' fontFamily='texturina' mb={6}>
+                Topics
+              </Heading>
+              <ul>
+                {Topics.map((topic) =>
+                  topic === '' ? (
+                    <li key={topic}>
+                      <Text
+                        mt={3}
+                        ml={4}
+                        onClick={() => router.push(`/state-of-the-raid`)}
+                        color={q ? 'white' : 'primary.500'}
+                        w='max-content'
+                        _hover={{ cursor: 'pointer', borderBottom: `1px solid white` }}>
+                        All Blogs
+                      </Text>
+                    </li>
+                  ) : (
+                    <li key={topic}>
+                      <Text
+                        mt={3}
+                        ml={4}
+                        w='max-content'
+                        onClick={() => router.push(`/state-of-the-raid?q=${topic.toLowerCase()}`)}
+                        color={String(q).toLowerCase() !== topic.toLowerCase() ? 'white' : 'primary.500'}
+                        _hover={{ cursor: 'pointer', borderBottom: `1px solid white` }}>
+                        {topic}
+                      </Text>
+                    </li>
+                  ),
+                )}
+              </ul>
+            </Box>
           </VStack>
-        </HStack>
+        </Stack>
         <PageEnd />
       </CMSPageTemplate>
     </Box>
