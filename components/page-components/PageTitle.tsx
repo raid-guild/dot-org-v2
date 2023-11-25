3b2c4fd6fc4bb8691c84c78c5b1b6f5a82d089e7
memosys <hello@vishal.design>
2023-11-25 17:28:14 +0530

    nearly done

--- components/page-components/PageTitle.tsx
+++ components/page-components/PageTitle.tsx
@@ -6,12 +6,13 @@ import RightWing from '../../assets/illustrations/RightWing.svg';
 
 interface PageTitleProps {
   title: string;
+  hideIcon?: boolean;
 }
 
-const PageTitle = ({ title }: PageTitleProps) => (
+const PageTitle = ({ title, hideIcon=false }: PageTitleProps) => (
   <Center py='2rem'>
     <VStack gap={8}>
-      <Image src={Swords.src} w='48px' h='48px' />
+      <Image src={Swords.src} w='48px' h='48px' hidden={hideIcon} />
       <HStack justifyContent='center'>
         <Image src={LeftWing.src} width={[`25vw`, `30vw`]} />
         <Heading variant='shadow' color='white' fontFamily='uncial' minW='200px' textAlign='center'>
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,5 +1,5 @@
 // A page that displays all of the projects in the portfolio
-import { Flex, Image, SimpleGrid, Stack, Text, defaultTheme } from '@raidguild/design-system';
+import { Box, Flex, Image, SimpleGrid, Stack, Text, VStack, defaultTheme } from '@raidguild/design-system';
 import _ from 'lodash';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
@@ -11,17 +11,50 @@ import CMSPageTemplate from '../../components/page-templates/CMSPageTemplate';
 import { getPortfolioList } from '../../gql';
 import usePortfolioList from '../../hooks/usePortfolioList';
 import { checkPermission } from '../../utils';
+import tokens from '../../utils/extendedTokens';
+
+const labels = [
+  'TOKEN',
+  'NFT DROP',
+  'LAUNCHPAD',
+  'DEFI',
+  'DAPP',
+  'COMMUNITY',
+  'NFT MARKET',
+  'DEX',
+  'PUBLIC GOODS',
+  'INFRASTRUCTURE',
+];
 
 interface Props {
   initialData: any;
 }
 
+const portfolioStats = [
+  {
+    numbers: '69+',
+    label: 'Unique Clients',
+  },
+  {
+    numbers: '120',
+    label: 'Raids Completed',
+  },
+  {
+    numbers: '54',
+    label: 'Active Raiders',
+  },
+  {
+    numbers: '54',
+    label: 'Active Raiders',
+  },
+];
+
 // a component for displaying a portfolio project in a list
 function PortfolioContent({ project }: { project: any }) {
   const link = `/portfolio/${project.slug}`;
   return (
-    <Link href={link} alignItems='center' justifyContent='center'>
-      <Image src={_.get(project, 'imageUrl')} maxH='60px' objectFit='cover' />
+    <Link href={link}>
+      <Image src={_.get(project, 'imageUrl')} maxH='40px' objectFit='cover' />
     </Link>
   );
 }
@@ -36,7 +69,7 @@ function PortfolioPage({ initialData }: Props) {
   return (
     <CMSPageTemplate>
       <PageTitle title='Portfolio' />
-      <Text textAlign='center' px='5rem'>
+      <Text textAlign='center' px='5rem' fontSize='xl' my={8} mx={4}>
         Our work speaks for itself. Click on one of our clients logos and see the project details page.
       </Text>
       {canCreate && (
@@ -52,17 +85,37 @@ function PortfolioPage({ initialData }: Props) {
           />
         </Stack>
       )}
+
       <SimpleGrid
-        spacing='60px'
+        m={12}
+        gap={{ base: 12, xl: 36 }}
+        columns={{ base: 2, xl: 4 }}
         alignItems='center'
-        justifyContent='center'
-        mx={8}
-        my={16}
-        columns={{ base: 1, md: 2, lg: 3 }}>
+        justifyContent='center'>
+        {portfolioStats.map((stat) => (
+          <VStack key={stat.label} spacing={1} textAlign='center'>
+            <Box bg={tokens.purpleToBlueGradient} bgClip='text'>
+              <Text fontSize='5xl' fontWeight='normal' fontFamily='uncial'>
+                {stat.numbers}
+              </Text>
+            </Box>
+            <Text fontSize='xl' fontWeight='medium'>
+              {stat.label}
+            </Text>
+          </VStack>
+        ))}
+      </SimpleGrid>
+      <Flex gap={8} alignItems='center' justifyContent='center' m={20} flexWrap='wrap' maxW='1200px'>
+        {labels.map((label) => (
+          <GradientBorderButton key={label} label={label} width='200px' />
+        ))}
+      </Flex>
+      <PageTitle title='' hideIcon />
+      <Flex gap={40} alignItems='center' justifyContent='center' mx={8} my={16} flexWrap='wrap'>
         {_.map(portfolioList, (project) => (
           <PortfolioContent project={project} key={_.get(project, 'name')} />
         ))}
-      </SimpleGrid>
+      </Flex>
     </CMSPageTemplate>
   );
 }
