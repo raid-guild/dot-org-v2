1df3ee1dea377e97b40c1dff5061ae7e3ec09dd9
memosys <82053242+memosys@users.noreply.github.com>
2023-12-07 15:27:49 +0530

    I won!!!

--- components/atoms/GradientBorderButton.tsx
+++ components/atoms/GradientBorderButton.tsx
@@ -1,4 +1,8 @@
+import React, { useState } from 'react';
 import { Box, Button } from '@raidguild/design-system';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const MotionBox = motion(Box);
 
 const GradientBorderButton = ({
   label,
@@ -49,28 +53,58 @@ const GradientBorderButton = ({
     backgroundColor: 'transparent',
   };
 
+  const [isHovered, setIsHovered] = useState(false);
+  const variants = {
+    initial: { width: 0 },
+    animate: {
+      width: '100%',
+      transition: { duration: 0.38 },
+    },
+    exit: {
+      width: 0,
+      transition: { duration: 0.2 },
+    },
+  };
+
   return (
     <Button
       variant='link'
       _hover={{
         textDecor: 'none',
-        _before: { content: 'none' },
-        _after: { content: 'none' },
-        backgroundSize: '100%',
         '.text-gradient': { bgClip: 'initial', textColor: '#fff' },
       }}
       height={10}
       onClick={onClick}
       ref={ref}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       _before={borderStyle('left', color1)}
       _after={borderStyle('right', color3)}
       sx={commonStyle}>
+      <AnimatePresence>
+        {isHovered && (
+          <MotionBox
+            position='absolute'
+            left={0}
+            top={0}
+            bottom={0}
+            height='100%'
+            borderRadius={2}
+            bg={linearGradient}
+            variants={variants}
+            initial='initial'
+            animate='animate'
+            exit='exit'
+          />
+        )}
+      </AnimatePresence>
       <Box
         className='text-gradient'
         bg={linearGradient}
         bgClip='text'
         w='100%'
         h='100%'
+        zIndex={10}
         padding='7px 24px'
         borderRadius={2}>
         {label}
