3c9114e9f9d4500c3b182c7ac8cb525f57121a00
memosys <82053242+memosys@users.noreply.github.com>
2024-01-15 18:46:44 +0530

    logo updates

--- components/landing/6-Supporters.tsx
+++ components/landing/6-Supporters.tsx
@@ -1,11 +1,14 @@
-import { Flex, Box, Text, Center, Link } from '@raidguild/design-system';
+import { Box, Center, Link, SimpleGrid, Text, Tooltip } from '@raidguild/design-system';
 import Image from 'next/image';
 
-import PageTitle from '../page-components/PageTitle';
-import metacartel from '../../assets/logos/metacartel.webp';
+import lexdao from '../../assets/logos/LexDAO-Logo.png';
+import tec from '../../assets/logos/TokenEngCommons.png';
 import daohaus from '../../assets/logos/daohaus.webp';
+import metacartel from '../../assets/logos/metacartel.webp';
+import metagame from '../../assets/logos/metagame.png';
 import moloch from '../../assets/logos/moloch.webp';
 import tokens from '../../utils/extendedTokens';
+import PageTitle from '../page-components/PageTitle';
 
 const SectionSix = () => {
   return (
@@ -15,35 +18,65 @@ const SectionSix = () => {
       flexDirection='column'
       alignItems='center'
       justifyContent='center'
-      padding={{ base: '2rem', lg: '10rem 4rem' }}
+      gap={10}
+      padding={{ base: '2rem', lg: '10rem' }}
       minH={{ base: 'max', md: '80vh' }}>
       <PageTitle title='Our Ecosystem' />
       <Text maxW='560px' textAlign='center' textColor='white' sx={{ whiteSpace: 'balance' }}>
         {`Raid Guild is a collective of developers with a multitude of talents building the future of the web. It wouldn't
         be possibly without the help of other DAOs such as..`}
       </Text>
-      <Flex
-        w='100%'
-        justifyContent='space-evenly'
+      <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3 }}
         alignItems='center'
-        mt={24}
-        direction={{ base: 'column', md: 'row', lg: 'row' }}>
+        justifyItems='center'
+        gap={{ base: 16, md: 20 }}
+        w='80%'>
         <Box w={{ base: '120px', lg: '200px' }}>
-          <Link href='https://www.metacartel.org/'>
-            <Image src={metacartel} alt='metacartel' placeholder='blur' />
-          </Link>
+          <Tooltip label='MetaCartel'>
+            <Link href='https://www.metacartel.org/'>
+              <Image src={metacartel} alt='metacartel' placeholder='blur' />
+            </Link>
+          </Tooltip>
         </Box>
         <Box w={{ base: '120px', lg: '200px' }}>
-          <Link href='https://daohaus.club/'>
-            <Image src={daohaus} alt='daohaus' placeholder='blur' />
-          </Link>
+          <Tooltip label='Daohaus'>
+            <Link href='https://daohaus.club/'>
+              <Image src={daohaus} alt='daohaus' placeholder='blur' />
+            </Link>
+          </Tooltip>
         </Box>
         <Box w={{ base: '120px', lg: '200px' }}>
-          <Link href='https://molochdao.com/'>
-            <Image src={moloch} alt='molochdao' placeholder='blur' />
-          </Link>
+          <Tooltip label='MolochDAO'>
+            <Link href='https://molochdao.com/'>
+              <Image src={moloch} alt='molochdao' placeholder='blur' />
+            </Link>
+          </Tooltip>
         </Box>
-      </Flex>
+
+        <Box w={{ base: '120px', lg: '160px' }}>
+          <Tooltip label='Metagame'>
+            <Link href='https://metagame.wtf/'>
+              <Image src={metagame} alt='metagame' placeholder='blur' />
+            </Link>
+          </Tooltip>
+        </Box>
+
+        <Box w={{ base: '120px', lg: '200px' }}>
+          <Tooltip label='Token Engineering Commons'>
+            <Link href='https://tecommons.org/'>
+              <Image src={tec} alt='tokenengineering' placeholder='blur' />
+            </Link>
+          </Tooltip>
+        </Box>
+        <Box w={{ base: '120px', lg: '160px' }}>
+          <Tooltip label='Lex DAO'>
+            <Link href='https://lexdao.org/'>
+              <Image src={lexdao} alt='lexdao' placeholder='blur' />
+            </Link>
+          </Tooltip>
+        </Box>
+      </SimpleGrid>
     </Center>
   );
 };
